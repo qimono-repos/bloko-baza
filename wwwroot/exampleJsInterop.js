6cb51a45a9cba551b48fd410b4a167d7d5bcf298
sat-lino-au <alberto.gruning.zen@gmai.com>
2025-03-14 14:23:01 +0000

    ci: first commit to add files into the repo

---
+++ wwwroot/exampleJsInterop.js
@@ -0,0 +1,5 @@
+// This is a JavaScript module that is loaded on demand. It can export any number of
+// functions, and may import other JavaScript modules if required.
+
+export const showPrompt(message)=> prompt(message, 'Type anything here')
+
